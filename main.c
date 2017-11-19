a63033320c02e6dce9ece5bdd87d9348a3d20f46
Gareth <garethjsharpe@gmail.com>
2017-11-19 15:37:11 -0500

    IT WORKED!

--- main.c
+++ main.c
@@ -90,14 +90,13 @@ int main(int argc, char *argv[])
 
   int data_length;
   if (process_rank == ROOT) {
-    printf("here");
     for (int rank = 0; rank < num_processors; rank++) {
-      printf("rank: %i", rank);
       MPI_Irecv(data_array, chunk + 1, MPI_INT, ROOT, rank, MPI_COMM_WORLD, &request);
       data_length = sizeof(data_array) / sizeof(int);
       for (int i = 0; i < data_length; i++) {
         printf("%i ", data_array[i]);
       }
+      printf("\n");
     }
   }
 
