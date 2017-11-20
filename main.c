1a48f47ca239248515993b2bfc1585bfe8387a29
Gareth <garethjsharpe@gmail.com>
2017-11-20 17:11:25 -0500

    %d

--- main.c
+++ main.c
@@ -65,7 +65,7 @@ int main(int argc, char *argv[])
   for (int i = 0; i < num_processors; ++i)
   {
       chunk_sizes[i] = floor((float) cells / num_processors);
-      printf("(float) cells / num_processors) %i", (float) cells / num_processors);
+      printf("(float) cells / num_processors) %d", (float) cells / num_processors);
     
       if (i < cells % num_processors)
         chunk_sizes[i] += 1;
