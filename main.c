0bd3e581a0781a4dc15280876f8a51b7b1fb7370
Gareth <garethjsharpe@gmail.com>
2017-11-19 22:35:57 -0500

    Root needs to send

--- main.c
+++ main.c
@@ -97,6 +97,9 @@ int main(int argc, char *argv[])
   {
     MPI_Send(&chunk, 1, MPI_LONG_LONG, ROOT, TAG_CHUNK_SIZE, MPI_COMM_WORLD);
     MPI_Send(data_array, chunk, MPI_LONG, ROOT, process_rank, MPI_COMM_WORLD);
+  } else
+  {
+    MPI_Isend(&chunk, 1, MPI_LONG_LONG, ROOT, TAG_CHUNK_SIZE, MPI_COMM_WORLD, &request);
   }
 
   if (process_rank == ROOT) 
