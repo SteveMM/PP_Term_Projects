0e11f88946579ad5d2a61d4b476b81aea8ae5a4e
BradleyKatz <brad.katz13@gmail.com>
2017-11-29 14:37:46 -0500

    Update main.c

--- main.c
+++ main.c
@@ -103,13 +103,13 @@ int main(int argc, char *argv[])
   printf("Filling bitmap\n");
   // Set the bit corrisponding to each product as a unique product
   unsigned long long int product = 0LL; 
-  while (my_chunk > 1LL) 
+  while (my_chunk > 0LL) 
   {
     product = i * j;
-    if (product > num_values) {
-      printf("Product greater than num values: %llu\n", product);
-      printf("i: %llu; j: %llu\n", i, j);
-    }
+    
+    if (product > num_values)
+      product = (i-1) * (j-1);
+    
     SETBIT(unique_bit_map, product);
     // printf("set %lli ", product);
     ++i; --my_chunk;
