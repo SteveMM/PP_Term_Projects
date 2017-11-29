255a6130d9cc7eae89e2dccc43645e05f1e1b2d7
BradleyKatz <brad.katz13@gmail.com>
2017-11-29 14:50:36 -0500

    Update main.c

--- main.c
+++ main.c
@@ -109,6 +109,7 @@ int main(int argc, char *argv[])
     
     if (product > num_values) {
       product = (i-1) * (j-1);
+      my_chunk = 0LL;
       printf("i: %llu; j: %llu; product: %llu Process: %d\n", i-1, j-1, product, process_rank);
     }
     
