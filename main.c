459800c6a4f83a3daa981fc0d7de3f6361701ee4
Gareth <garethjsharpe@gmail.com>
2017-12-05 19:57:15 -0500

    Test

--- main.c
+++ main.c
@@ -111,15 +111,13 @@ int main(int argc, char *argv[])
     }
   }
   
-  const unsigned int half_size = (int) ceil((float)ints_in_bitarray / 2.0f) + 1;
+  const unsigned int half_size = (int) ceil((float)ints_in_bitarray / 2.0f);
   
   // Each process sends it's unique bitmap to the root process
   if (process_rank != ROOT) 
   { 
     MPI_Send(unique_bit_map, half_size, MPI_UNSIGNED, ROOT, TAG_BIT_MAP, MPI_COMM_WORLD);
-    
-    if (half_size > 1)
-      MPI_Send(unique_bit_map + half_size, half_size, MPI_UNSIGNED, ROOT, TAG_BIT_MAP, MPI_COMM_WORLD);
+    MPI_Send(unique_bit_map + half_size, half_size, MPI_UNSIGNED, ROOT, TAG_BIT_MAP, MPI_COMM_WORLD);
     
     free(unique_bit_map);
   }
@@ -137,13 +135,11 @@ int main(int argc, char *argv[])
         for (int i = 0; i < half_size; ++i)
           unique_bit_map[i] |= incoming_bit_map[i];
       
-        if (half_size > 1)
-        {
-          MPI_Recv(incoming_bit_map, half_size, MPI_UNSIGNED, rank, TAG_BIT_MAP, MPI_COMM_WORLD, NULL);
+        MPI_Recv(incoming_bit_map, half_size, MPI_UNSIGNED, rank, TAG_BIT_MAP, MPI_COMM_WORLD, NULL);
 
-          for (int i = 0; i < half_size; ++i)
-              (unique_bit_map + half_size)[i] |= incoming_bit_map[i];
-        }
+        for (int i = 0; i < half_size; ++i)
+            (unique_bit_map + half_size)[i] |= incoming_bit_map[i];
+
     }
     
       // Free the incoming bitmap space
